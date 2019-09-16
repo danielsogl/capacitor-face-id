ad0d03bfaa66912172f7979e7b1cff724304b9e9
Daniel Sogl <mytechde@outlook.com>
2019-09-16 16:31:15 +0200

    docs: rework interfaces

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,25 +6,28 @@ declare module '@capacitor/core' {
 }
 
 export interface FaceIdPluginIsAvailableResult {
-  /* true if Face ID or Touch ID is available */
+  /** true if Face ID or Touch ID is available */
   value: boolean;
 }
 
 export interface FaceIdPluginAuthOptions {
-  /* String to display authentication reason */
+  /** String to display authentication reason */
   reason?: string;
 }
 
 export interface FaceIdPlugin {
   /**
    * check if Face ID or Touch ID is available
-   * @returns true if available
+   * @returns  {Promise}
+   * @resolve {value: boolean}
+   * @rejects PluginResultError
    */
   isAvailable(): Promise<FaceIdPluginIsAvailableResult>;
   /**
    * Displays the authentication screen
-   * @returns {Promise<void>}
-   * @throws {PluginResultError}
+   * @returns  {Promise}
+   * @resolve void
+   * @rejects PluginResultError
    */
   auth(options?: FaceIdPluginAuthOptions): Promise<void>;
 }
