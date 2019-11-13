c611f57cb3207a1ca1673ecc2b6a6bd85037edc2
mkw <michaelkweiner@gmail.com>
2019-11-13 07:30:49 -0800

    feat(plugin): return available type instead of boolean  (#10)
    * returning type instead of boolean
    
    * update readme
    
    * comment
    
    * double quotes

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,8 +6,8 @@ declare module '@capacitor/core' {
 }
 
 export interface FaceIdPluginIsAvailableResult {
-  /** true if Face ID or Touch ID is available */
-  value: boolean;
+  /**  Returns available biometric type or 'None' if unavailable */
+  value: string;
 }
 
 export interface FaceIdPluginAuthOptions {
@@ -19,7 +19,7 @@ export interface FaceIdPlugin {
   /**
    * check if Face ID or Touch ID is available
    * @returns  {Promise}
-   * @resolve {value: boolean}
+   * @resolve {value: string}
    * @rejects PluginResultError
    */
   isAvailable(): Promise<FaceIdPluginIsAvailableResult>;
