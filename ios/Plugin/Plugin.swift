c9c51ae10cc0de23bb7df1ab4855e7a250c43b60
Aparajita Fishman <aparajita@aparajita.com>
2020-07-01 13:11:46 -0700

    feat: support more configuration

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -2,55 +2,75 @@ import Foundation
 import Capacitor
 import LocalAuthentication
 
+let kNoneType = "None"
+let kTouchIDType = "Touch ID"
+let kFaceIDType = "Face ID"
+
+let errorCodeMap: [LAError.Code: String] = [
+  .appCancel: "appCancel",
+  .authenticationFailed: "authenticationFailed",
+  .invalidContext: "invalidContext",
+  .notInteractive: "notInteractive",
+  .passcodeNotSet: "passcodeNotSet",
+  .systemCancel: "systemCancel",
+  .userCancel: "userCancel",
+  .userFallback: "userFallback",
+  .biometryLockout: "biometryLockout",
+  .biometryNotAvailable: "biometryNotAvailable",
+  .biometryNotEnrolled: "biometryNotEnrolled"
+]
+
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
 @objc(FaceId)
 public class FaceId: CAPPlugin {
-    
-    @objc func isAvailable(_ call: CAPPluginCall) {
-        if #available(iOS 11, *) {
-            let authContext = LAContext()
-            
-            let _ = authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
-            switch(authContext.biometryType) {
-            case .none:
-                call.success([
-                    "value": "None"
-                ])
-            case .touchID:
-                call.success([
-                    "value": "TouchId"
-                ])
-            case .faceID:
-                call.success([
-                    "value": "FaceId"
-                ])
-            }
-        } else {
-            call.success([
-                "value": "None"
-            ])
-        }
+  
+  @objc func isAvailable(_ call: CAPPluginCall) {
+    var authType = kNoneType
+
+    if #available(iOS 11, *) {
+      let authContext = LAContext()
+      
+      let _ = authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+      
+      switch (authContext.biometryType) {
+      case .touchID:
+        authType = kTouchIDType
+        
+      case .faceID:
+        authType = kFaceIDType
+        
+      default:
+        break
+      }
     }
     
-    @objc func auth(_ call: CAPPluginCall) {
-        let authContext = LAContext()
-        
-        authContext.touchIDAuthenticationAllowableReuseDuration = 60;
-        
-        let reason = call.getString("reason") ?? "Access requires authentication"
-        authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason ) { success, error in
-            if success {
-                DispatchQueue.main.async {
-                    call.success()
-                }
-            } else {
-                call.error(error?.localizedDescription ?? "Failed to authenticate")
-            }
+    call.success([
+      "value": authType
+    ])
+  }
+  
+  @objc func auth(_ call: CAPPluginCall) {
+    let authContext = LAContext()
+    
+    authContext.localizedFallbackTitle = call.getString("fallbackTitle")
+    authContext.touchIDAuthenticationAllowableReuseDuration = call.getDouble("touchIDReuseDuration") ?? 60
+    authContext.localizedCancelTitle = call.getString("cancelTitle")
+    let reason = call.getString("reason") ?? "Access requires authentication"
+    
+    authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason ) {
+      success, error in
+      if success {
+        DispatchQueue.main.async {
+          call.resolve()
         }
+      } else {
+        let policyError = error as! LAError
+        let code = errorCodeMap[policyError.code]
+        call.reject(policyError.localizedDescription, code)
+      }
     }
-    
-    
+  }
 }
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,6 @@
 // @ts-ignore
+import { PluginResultError } from '@capacitor/core'
+
 declare module '@capacitor/core' {
   interface PluginRegistry {
     FaceId: FaceIdPlugin;
@@ -7,12 +9,61 @@ declare module '@capacitor/core' {
 
 export interface FaceIdPluginIsAvailableResult {
   /**  Returns available biometric type or 'None' if unavailable */
-  value: 'None' | 'TouchId' | 'FaceId';
+  value: 'None' | 'Touch Id' | 'Face Id';
 }
 
 export interface FaceIdPluginAuthOptions {
-  /** String to display authentication reason */
+  /**
+   * Localized string to display as the authentication reason.
+   * Defaults to "Access requires authentication".
+   */
   reason?: string;
+
+  /**
+   * The localized title for the fallback button.
+   * An empty string removes the button.
+   * Omitting this shows the iOS default.
+   */
+  fallbackTitle?: string;
+
+  /**
+   * See https://developer.apple.com/documentation/localauthentication/lacontext/1622329-touchidauthenticationallowablere
+   * Defaults to 60.
+   */
+  touchIDReuseDuration: number;
+
+  /**
+   * The localized title for the cancel button in the dialog
+   * presented to the user during authentication. Omitting this
+   * or passing an empty string displays the iOS default.
+   */
+  localizedCancelTitle?: string;
+}
+
+/**
+ * If the auth() function throws an exception, the error object
+ * contains a .code property which will contain one of these strings,
+ * indicating what the error was.
+ *
+ * See https://developer.apple.com/documentation/localauthentication/laerror
+ * for a description of each error code.
+ */
+export enum FaceIDPluginErrorCode {
+  appCancel = "appCancel",
+  authenticationFailed = "authenticationFailed",
+  invalidContext = "invalidContext",
+  notInteractive = "notInteractive",
+  passcodeNotSet = "passcodeNotSet",
+  systemCancel = "systemCancel",
+  userCancel = "userCancel",
+  userFallback = "userFallback",
+  biometryLockout = "biometryLockout",
+  biometryNotAvailable = "biometryNotAvailable",
+  biometryNotEnrolled = "biometryNotEnrolled"
+}
+
+export interface FaceIDPluginResultError extends PluginResultError {
+  code: FaceIDPluginErrorCode
 }
 
 export interface FaceIdPlugin {
@@ -27,7 +78,7 @@ export interface FaceIdPlugin {
    * Displays the authentication screen
    * @returns  {Promise}
    * @resolve void
-   * @rejects PluginResultError
+   * @rejects FaceIDPluginResultError
    */
   auth(options?: FaceIdPluginAuthOptions): Promise<void>;
 }
